6d515657f7d8ea009bbc10fea0ac42a6a39e572c
NeilRow56 <neilrowland56@gmail.com>
2025-06-10 16:50:59 +0100

    Added dashboard layout, sites page and empty state component

---
+++ src/app/dashboard/sites/new/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NewSitePage = () => {
+  return <div>New Site Page</div>
+}
+
+export default NewSitePage
--- src/app/dashboard/sites/page.tsx
+++ src/app/dashboard/sites/page.tsx
@@ -1,7 +1,28 @@
+import { EmptyState } from '@/components/dashboard/empty-state'
+import { Button } from '@/components/ui/button'
+import { PlusCircle } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 const SitesPage = () => {
-  return <div>Sites Page</div>
+  return (
+    <>
+      <div className='flex w-full justify-end'>
+        <Button asChild>
+          <Link href={'/dashboard/sites/new'}>
+            <PlusCircle className='mr-2 size-4' /> Create Site
+          </Link>
+        </Button>
+      </div>
+      <EmptyState
+        title='You dont have any Sites created'
+        description='You currently dont have any Sites. Please create some so that you can
+        see them right here!'
+        buttonText='Create Site'
+        href='/dashboard/sites/new'
+      />
+    </>
+  )
 }
 
 export default SitesPage
